e64adc8f1aebda1dd5e67eef887cd91661e4b883
Prince singh <111722098+princesingh04@users.noreply.github.com>
2022-12-25 17:44:23 +0530

    my portfolio
    my portfolio website

---
+++ c++/1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node* next;
+
+Node(int data){
+    cout<<"constructor called"<<endl;
+    this->data=data;
+    this->next=NULL;
+}
+
+};
+int main(){
+
+
+Node *node1= new Node(15);
+cout<<node1->data<<endl;
+cout<<node1->next<<endl;
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
